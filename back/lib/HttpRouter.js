b8d0e36da4fb1eb39104c4ea3f990d53fac5afcd
Dmitry Ovsyanko <do_@pochta.ru>
2020-06-28 21:02:42 +0300

    optimization

--- back/lib/HttpRouter.js
+++ back/lib/HttpRouter.js
@@ -4,8 +4,18 @@ const front = require ('./Ext/Dia/Content/Handler/HTTP/EluStatic.js')
 module.exports = class extends require ('./Ext/Dia/Content/Handler/HTTP/Router.js') {
 
 	create_http_handler (http) {
+	
+		let {url} = http.request
 
-		if (http.request.url.match (/^\/(\?|_back)/)) return new back ({conf: this.conf, pools: this.conf.pools, http})
+		if (url.match (/^\/(\?|_back)/)) {
+		
+			let {conf} = this, pools = {...conf.pools}
+
+			if (!url.match (/type=table_data/)) delete pools.db_o
+				
+			return new back ({conf, pools, http})
+		
+		}
 
 		return new front ({http})
 

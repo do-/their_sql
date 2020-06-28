7e5f1d48e47a45ffdbec0f27293bb04ede183b5e
Dmitry Ovsyanko <do_@pochta.ru>
2020-06-28 12:22:14 +0300

    col hrefs

--- front/root/_/app/js/view/columns.js
+++ front/root/_/app/js/view/columns.js
@@ -20,7 +20,7 @@ $_DRAW.columns = async function (data) {
     
         columns: [
         
-            {field: 'id_table', caption: 'Таблица',    size: 50, sortable: true},
+            {field: 'id_table', caption: 'Таблица',    size: 50, sortable: true, attr: 'data-ref=1'},
             {field: 'name',     caption: 'Поле',    size: 50, sortable: true},
 
             {field: 'type',     caption: 'Тип',    size: 50},
@@ -29,7 +29,7 @@ $_DRAW.columns = async function (data) {
             {field: 'remark',   caption: 'Их',    size: 100},
             {field: 'note',     caption: 'Наш',    size: 100, editable: {type: 'text'}},
             
-            {field: 'id_ref_table',    caption: 'Имя',    size: 50, sortable: true},
+            {field: 'id_ref_table',    caption: 'Имя',    size: 50, attr: 'data-ref=1'},
             {field: 'tables.remark',   caption: 'Их комментарий',     size: 100},
             {field: 'tables.note',     caption: 'Наш комментарий',    size: 100},
             
@@ -40,6 +40,18 @@ $_DRAW.columns = async function (data) {
         onDblClick: null,
         
 		onChange: $_DO.patch_columns,        
+
+        onClick: function (e) {
+        
+        	let r = this.get (e.recid), {field} = this.columns [e.column]
+        
+        	switch (field) {
+        		case 'id_table':
+        		case 'id_ref_table':
+        			 open_tab (`/table/${r [field]}`)
+        	}
+        
+        },
 
     }).refresh ();
     

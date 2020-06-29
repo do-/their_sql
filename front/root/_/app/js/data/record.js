036eb54dfefaa46405e572032eae096094201682
Dmitry Ovsyanko <do_@pochta.ru>
2020-06-30 00:21:30 +0300

    record selection FIX

--- front/root/_/app/js/data/record.js
+++ front/root/_/app/js/data/record.js
@@ -8,7 +8,7 @@ $_GET.record = async function (o) {
 
     let {columns} = await response ({type: 'columns', id: null}, {id_table, offset: 0, limit: 10000})
 
-	let {all}     = await response ({type: 'table_data', id: null}, {id_table, offset: 0, limit: 1, search: [{field: table.pk, type: "int", operator: "is", value: id}]})
+	let {all}     = await response ({type: 'table_data', id: null}, {id_table, offset: 0, limit: 1, search: [{field: table.pk, type: "int", operator: "is", value: id}], searchLogic: "AND"})
 	
 	let r = {}; for (let [k, v] of Object.entries (all [0])) if (v != null) r [k.toLowerCase ()] = v
 	

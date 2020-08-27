55e7773cb379d64b362c53d735a6d7236eeab9b5
Dmitry Ovsyanko <do_@pochta.ru>
2020-08-27 09:54:37 +0300

    mysql import optimization

--- back/lib/Content/tables.js
+++ back/lib/Content/tables.js
@@ -1,4 +1,5 @@
 const fs = require ('fs')
+const {Readable, Transform, PassThrough} = require ('stream')
 
 module.exports = {
 
@@ -67,36 +68,53 @@ do_reload_oviont_tables:
     async function () {
     
     	let {db, db_o} = this
+    	
+    	async function imp (table, cols, sql) {
 
-    	await db.do ('UPDATE tables SET is_confirmed = 0 WHERE id SIMILAR TO ?', ['(fkr|fkr_rr|mkd_service).%'])
+	    	let tmp = await db.create_temp_as (table, cols)
+	    	let str = await db_o.select_stream (sql)
+	    	
+			await db.load (str, tmp, cols)
 
-  		await db.upsert ('tables', await db_o.select_all (`
+			await db.do (`
+				INSERT INTO ${table} (${cols}) 
+				SELECT ${cols}
+				FROM ${tmp}
+				ON CONFLICT (id) DO UPDATE SET ${cols.filter (k => k != 'id').map (k => k + '=EXCLUDED.' + k)}
+			`)
+
+			await db.do (`UPDATE ${table} SET is_confirmed = 0 WHERE id SIMILAR TO ? AND id NOT IN (SELECT id FROM ${tmp})`, ['(fkr|fkr_rr|mkd_service).%'])
+			await db.do (`UPDATE ${table} SET is_confirmed = 1 WHERE id IN (SELECT id FROM ${tmp})`)
+
+    	}
+    	
+    	await imp ('tables', ['id', 'is_view', 'cnt', 'remark'], `
+    	
 			SELECT
-			  CONCAT(t.table_schema, '.', t.table_name) id
-			  , CASE t.table_type WHEN 'VIEW' THEN 1 ELSE 0 END is_view
-			  , IFNULL(t.table_rows, 0) cnt
-			  , CASE t.table_comment WHEN '' THEN NULL ELSE t.table_comment END remark
-			  , 1 is_confirmed
+				CONCAT(t.table_schema, '.', t.table_name) id
+				, CASE t.table_type WHEN 'VIEW' THEN 1 ELSE 0 END is_view
+				, IFNULL(t.table_rows, 0) cnt
+				, CASE t.table_comment WHEN '' THEN NULL ELSE t.table_comment END remark
 			FROM
-			  information_schema.tables t
-			WHERE 1=1
-			  AND t.table_schema IN ('fkr', 'fkr_rr', 'mkd_service')
-  		`))
+				information_schema.tables t
+			WHERE
+				t.table_schema IN ('fkr', 'fkr_rr', 'mkd_service')
+    	
+    	`)
 
-    	await db.do ('UPDATE columns SET is_confirmed = 0 WHERE id SIMILAR TO ?', ['(fkr|fkr_rr|mkd_service).%'])
-
-  		await db.upsert ('columns', await db_o.select_all (`
+    	await imp ('columns', ['id', 'is_pk', 'type', 'remark'], `
+    	
 			SELECT
-			  CONCAT(t.table_schema, '.', t.table_name, '.', LOWER(t.column_name)) id
-			  , CASE t.column_key WHEN 'PRI' THEN 1 ELSE 0 END is_pk
-			  , t.column_type type
-			  , CASE t.column_comment WHEN '' THEN NULL ELSE t.column_comment END remark
-			  , 1 is_confirmed
+				CONCAT(t.table_schema, '.', t.table_name, '.', LOWER(t.column_name)) id
+				, CASE t.column_key WHEN 'PRI' THEN 1 ELSE 0 END is_pk
+				, t.column_type type
+				, CASE t.column_comment WHEN '' THEN NULL ELSE t.column_comment END remark
 			FROM
-			  information_schema.columns t
-			WHERE 1=1
-			  AND t.table_schema IN ('fkr', 'fkr_rr', 'mkd_service')
-  		`))
+				information_schema.columns t
+			WHERE
+				t.table_schema IN ('fkr', 'fkr_rr', 'mkd_service')
+    	
+    	`)
 
     },
 
